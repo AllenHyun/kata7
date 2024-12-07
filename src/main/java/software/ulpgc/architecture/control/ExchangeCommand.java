3993a017944c0f0290b05d864cd806c988100ab9
maria <informatica1bachIES@gmail.com>
2024-12-07 12:11:00 +0000

    added the interface command to execute our code and the class ExchangeCommand

---
+++ src/main/java/software/ulpgc/architecture/control/ExchangeCommand.java
@@ -0,0 +1,29 @@
+package software.ulpgc.architecture.control;
+
+import software.ulpgc.architecture.model.ExchangeRate;
+import software.ulpgc.architecture.model.Money;
+
+import java.time.LocalDate;
+
+public class ExchangeCommand implements Command{
+    private final MoneyDialog moneyDialog;
+    private final CurrencyDialog currencyDialog;
+    private final ExchangeRateLoader loader;
+    private final MoneyDisplay moneyDisplay;
+
+    public ExchangeCommand(MoneyDialog moneyDialog, CurrencyDialog currencyDialog, ExchangeRateLoader loader, MoneyDisplay moneyDisplay) {
+        this.moneyDialog = moneyDialog;
+        this.currencyDialog = currencyDialog;
+        this.loader = loader;
+        this.moneyDisplay = moneyDisplay;
+    }
+
+    @Override
+    public void execute() {
+        Money money = moneyDialog.get();
+        Currency currency = currencyDialog.get();
+        ExchangeRate exchangeRate = loader.load(money.currency(), currency, LocalDate.now());
+        Money result = new Money(money.amount() * exchangeRate.rate(), currency);
+        moneyDisplay.show(result);
+    }
+}
