eafff1d36fb8cc050fa97bbf7f739cfc91be62c0
maria <informatica1bachIES@gmail.com>
2024-12-07 13:13:48 +0000

    added mainFrame

--- src/main/java/software/ulpgc/apps/windows/Main.java
+++ src/main/java/software/ulpgc/apps/windows/Main.java
@@ -1,4 +1,21 @@
 package software.ulpgc.apps.windows;
 
+import software.ulpgc.apps.mock.MockCurrencyLoader;
+import software.ulpgc.apps.mock.MockExchangeRateLoader;
+import software.ulpgc.architecture.control.ExchangeCommand;
+import software.ulpgc.architecture.model.Currency;
+
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Currency> currencies = new MockCurrencyLoader().load();
+        MainFrame mainFrame = new MainFrame(currencies);
+        ExchangeCommand exchangeCommand = new ExchangeCommand(
+                mainFrame.moneyDialog(),
+                mainFrame.currencyDialog(),
+                new MockExchangeRateLoader(),
+                mainFrame.moneyDisplay()
+        );
+    }
 }
---
+++ src/main/java/software/ulpgc/apps/windows/MainFrame.java
@@ -0,0 +1,80 @@
+package software.ulpgc.apps.windows;
+
+import software.ulpgc.apps.windows.view.SwingCurrencyDialog;
+import software.ulpgc.apps.windows.view.SwingMoneyDialog;
+import software.ulpgc.apps.windows.view.SwingMoneyDisplay;
+import software.ulpgc.architecture.control.Command;
+import software.ulpgc.architecture.model.Currency;
+import software.ulpgc.architecture.view.CurrencyDialog;
+import software.ulpgc.architecture.view.MoneyDialog;
+import software.ulpgc.architecture.view.MoneyDisplay;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainFrame extends JFrame {
+    private final List<Currency> currencies;
+    private final SwingMoneyDialog moneyDialog;
+    private final SwingCurrencyDialog currencyDialog;
+    private final SwingMoneyDisplay moneyDisplay;
+    private final Map<String, Command> commands;
+
+    public MainFrame(List<Currency> currencies){
+        this.currencies = currencies;
+        this.setTitle("Money Calculator");
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setLayout(new FlowLayout());
+        this.add(moneyDialog = createMoneyDialog());
+        this.add(currencyDialog = createCurrencyDialog());
+        this.add(moneyDisplay = createMoneyDisplay());
+        this.add(createCalculateButton());
+        this.commands = new HashMap<>();
+    }
+
+    private Component createCalculateButton() {
+        JButton button = new JButton("Calculate");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commands.get("exchange").execute();
+            }
+        });
+        return button;
+    }
+
+    private SwingMoneyDisplay createMoneyDisplay() {
+        return new SwingMoneyDisplay();
+    }
+
+    private SwingCurrencyDialog createCurrencyDialog() {
+        return new SwingCurrencyDialog(currencies);
+    }
+
+    private SwingMoneyDialog createMoneyDialog() {
+        return new SwingMoneyDialog(currencies);
+    }
+
+    public MoneyDialog moneyDialog(){
+        return moneyDialog;
+    }
+
+    public CurrencyDialog currencyDialog() {
+        return currencyDialog;
+    }
+
+    public MoneyDisplay moneyDisplay() {
+        return moneyDisplay;
+    }
+
+    public void add(String operation, Command command){
+        commands.put(operation, command);
+    }
+
+}
